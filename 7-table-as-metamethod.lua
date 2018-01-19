39b23cc22d25501bdf268001811fa619183abd7f
caigen <caigen@outlook.com>
2018-01-19 14:53:06 +0800

    __newindex uses table also.

--- 7-table-as-metamethod.lua
+++ 7-table-as-metamethod.lua
@@ -9,10 +9,26 @@ inner_table = {
 }
 
 metatable = {
-	__index = inner_table
+	__index = inner_table,
+	__newindex = inner_table,
 }
 
 table1 = { }
 setmetatable(table1, metatable)
-print(table1.x, table1.y, table1.z, table1.msg, table1:msg())
+print(table1, table1.x, table1.y, table1.z, table1.msg, table1:msg())
 
+table2 = { }
+setmetatable(table2, metatable)
+print(table2, table2.x, table2.y, table2.z, table2.msg, table2:msg())
+
+print("change table1 only")
+table1.x = 2
+table1.y = 2
+table1.z = 2
+table1.msg = 
+	function()
+		return "updated"
+	end
+
+print(table1, table1.x, table1.y, table1.z, table1.msg, table1:msg())
+print(table2, table2.x, table2.y, table2.z, table2.msg, table2:msg())
